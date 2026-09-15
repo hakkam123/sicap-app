4e524d9283dcf559502156628b2060a1bf780c26
hakkam123 <hakkamrobbani@gmail.com>
2026-09-15 15:54:27 +0700

    feat(branding): update browser tab favicon and app.blade.php with official COPA logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,11 @@
 
         <title inertia>{{ config('app.name', 'COPA - Consumption Part Application') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}?v=2">
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=2">
+        <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}?v=2">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
